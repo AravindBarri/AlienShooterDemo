4f70d3164b09e81f8c7b5def3de65d34a7aa4f6f
PTRL291\Aravind Barri <aravindbarri@gmail.com>
2021-09-07 15:35:01 +0530

    player camera adjusted with free view camera

--- Assets/thirdpersonShooter/Scripts/GunController.cs
+++ Assets/thirdpersonShooter/Scripts/GunController.cs
@@ -14,20 +14,10 @@ public class GunController : MonoBehaviour
     [SerializeField] float timer;
     [SerializeField] Transform firePoint;
     public ParticleSystem particleSyste;
-    //public GameObject bulletPrefab;
-    //[SerializeField] float bulletSpeed;
-    // AudioSource audioSource;
-    // public AudioClip audioClip;
+
     [SerializeField]
     int TotalEnemies = 4;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-       // audioSource = GetComponent<AudioSource>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
@@ -35,36 +25,19 @@ public class GunController : MonoBehaviour
         {
             if (Input.GetButton("Fire1"))
             {
-                //Instantiate(particleSyste, particlePoint.position, Quaternion.identity);
-                //BulletSpawn();
-              //  audioSource.clip = audioClip;
-               // audioSource.Play();
                 timer = 0f;
                 FireGun();
                 particleSyste.Play();
             }
+            
         }
-        if(TotalEnemies == 0)
+        if (TotalEnemies == 0)
         {
             HelicopterSpawn.helicinstance.finishedGame = true;
         }
     }
-
-    /*public void BulletSpawn()
-    {
-        GameObject temp = Instantiate(bulletPrefab);
-        temp.transform.position = firePoint.transform.position;
-        temp.GetComponent<Rigidbody>().AddForce(Vector3.forward*bulletSpeed);
-    }*/
     private void FireGun()
     {
-
-        //particleSyste.transform.position = particlePoint.position;
-        //particleSyste.Play();
-
-        //Debug.DrawRay(firePoint.position, firePoint.forward*50f, Color.red,2f);
-        //Ray ray = new Ray(firePoint.position, firePoint.forward);
-        //Ray ray = Camera.main.ViewportPointToRay(Vector3.one * 0.5f);
         Ray ray = new Ray(firePoint.position, firePoint.right);
         Debug.DrawRay(ray.origin, ray.direction * 30f, Color.blue, 2f);
         RaycastHit hit;
--- Assets/thirdpersonShooter/Scripts/PlayerMovement.cs
+++ Assets/thirdpersonShooter/Scripts/PlayerMovement.cs
@@ -11,8 +11,11 @@ public class PlayerMovement : MonoBehaviour
     Animator anim;
     ScoreManager score;
     public GameObject ScoreM;
+    public Transform cam;
+    private float rotateXspeed = 4.0f;
 
-
+    public float smoothtime = 0.1f;
+    float turnSmoothVelocity;
    
     private void Awake()
     {
@@ -22,15 +25,26 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
+        
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
+        var mouseHorizontal = Input.GetAxis("Mouse X");
+        var mouseVectical = Input.GetAxis("Mouse Y");
         var movement = new Vector3(horizontal, 0, vertical);
-        anim.SetFloat("Speed", vertical);
-        transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
-        if (vertical != 0)
+        anim.SetFloat("Speed", movement.magnitude);
+        this.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + mouseHorizontal * rotateXspeed, transform.localEulerAngles.z);
+
+        //transform.Rotate(Vector3.up, horizontal * turnSpeed * Time.deltaTime);
+        if (movement.magnitude >=0.1f)
         {
+            print(movement.magnitude);
+            float targetangle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg +cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetangle, ref turnSmoothVelocity,smoothtime);
+            transform.rotation = Quaternion.Euler(0f, targetangle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetangle, 0f) * Vector3.forward;
             float moveSpeed = (vertical > 0) ? playerSpeed : backSpeed;
-            characterController.SimpleMove(transform.forward * vertical * moveSpeed);
+            characterController.Move( moveDir.normalized * moveSpeed * Time.deltaTime);
             //audioSource.clip = audioClip;
             //audioSource.Play();
         }
